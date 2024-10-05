9187d1ec0cf14fb50959c7b7f900dcdb68c20cf3
nf-714 <nfuad714@gmail.com>
2024-10-05 18:01:27 +0600

    fixing deployment issue

--- auth/auth.config.ts
+++ auth/auth.config.ts
@@ -1,30 +1,6 @@
-import { findUser } from "@/queries/user/user.queries";
-import { LoginSchema } from "@/schemas";
-import bcrypt from "bcryptjs";
-import { NextAuthConfig } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import google from "next-auth/providers/google";
-
-const credentialProvider = {
-  credentials: {
-    email: {},
-    password: {},
+export const authConfig = {
+  session: {
+    strategy: "jwt",
   },
-  async authorize(credentials: unknown) {
-    const validatedFields = LoginSchema.safeParse(credentials);
-    if (validatedFields.success) {
-      const user = await findUser(credentials.email);
-      if (!user || !user.password) return null;
-
-      const passwordMatch = await bcrypt.compare(password, user.password);
-
-      if (passwordMatch) return user;
-
-      return null;
-    }
-  },
+  providers: [],
 };
-
-export default {
-  providers: [google, CredentialsProvider(credentialProvider)],
-} satisfies NextAuthConfig;
--- auth/auth.ts
+++ auth/auth.ts
@@ -1,10 +1,34 @@
 import clientPromise from "@/db/adapter-mongodb";
+import { findUser } from "@/queries/user/user.queries";
+import { LoginSchema } from "@/schemas";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import bcrypt from "bcryptjs";
 import NextAuth from "next-auth";
-import authConfig from "./auth.config";
+import CredentialsProvider from "next-auth/providers/credentials";
+import google from "next-auth/providers/google";
+
+const credentialProvider = {
+  credentials: {
+    email: {},
+    password: {},
+  },
+  async authorize(credentials: unknown) {
+    const validatedFields = LoginSchema.safeParse(credentials);
+    if (validatedFields.success) {
+      const user = await findUser(credentials.email);
+      if (!user || !user.password) return null;
+
+      const passwordMatch = await bcrypt.compare(password, user.password);
+
+      if (passwordMatch) return user;
+
+      return null;
+    }
+  },
+};
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  ...authConfig,
+  providers: [google, CredentialsProvider(credentialProvider)],
   adapter: MongoDBAdapter(clientPromise),
   session: {
     strategy: "jwt",
